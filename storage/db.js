c63ae335e9e1100e433398ef59a5b74642a6be15
Gourav1221 <90262724+Gourav1221@users.noreply.github.com>
2022-07-23 14:21:32 +0530

    react with ts

--- storage/db.js
+++ storage/db.js
@@ -1,4 +1,6 @@
-const mongoose= require("mongoose")
-module.exports=()=>{
-    return mongoose.connect("mongodb+srv://gourab:gourab111@cluster0.ueyki.mongodb.net/?retryWrites=true&w=majority")
-}
+const mongoose = require("mongoose");
+module.exports = () => {
+  return mongoose.connect(
+    "mongodb+srv://gourab:gourab111@cluster0.ueyki.mongodb.net/?retryWrites=true&w=majority"
+  );
+};
