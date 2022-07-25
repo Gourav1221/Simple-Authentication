cfc72bc9c4e458168790b884978710de05f5055c
Gourav1221 <90262724+Gourav1221@users.noreply.github.com>
2022-07-26 00:57:51 +0530

    little bit change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/", function(req, res){
     return res.send("hello welcome")
 })
 
-const port = process.env.port || 8080
+const port = process.env.PORT || 8080
 
 
 app.listen(port,async ()=>{
